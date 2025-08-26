e9abb1c503751c4c5f7e1346b63f237126892926
Peng Zhao <51162669+biozhp@users.noreply.github.com>
2025-08-26 17:10:49 +0800

    Update TPM_qqnorm.R

--- TPM_qqnorm.R
+++ TPM_qqnorm.R
@@ -1,6 +1,8 @@
-gene <- read.table("exp_input_qqnorm.txt",header=T,row.names = 1,sep="\t")
+## The data matrix is assumed to have N rows and G columns, where N is the number of geness, and G is the number of samples.
+df <- read.table("TPM.txt",header=T,row.names = 1,sep="\t")
+gene <- data.frame(t(df))
 mydata<-list()
 for (i in seq_along(gene)){
- mydata[[i]] <- (qqnorm(gene[[i]]))$x
+  mydata[[i]] <- (qqnorm(gene[[i]]))$x
 }
-write.table(mydata,file="qqnorm.txt",quote=F,row.names=T,col.names=T,sep="\t")
+write.table(mydata,file="qqnorm.txt",quote=F,row.names=F,col.names=F,sep="\t")
